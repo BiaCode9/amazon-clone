be8e06a98da0579a02976b0515d8ae9996dddc98
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-09-22 23:30:44 -0400

    added payment page method update

--- src/Payment.js
+++ src/Payment.js
@@ -1,9 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import CheckoutProduct from "./CheckoutProduct";
 import "./Payment.css";
 import { useStateValue } from "./StateProvider";
 import { Link } from "react-router-dom";
 import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
+import CurrencyFormat from "react-currency-format";
+import { getBasketTotal } from "./reducer.js";
 
 function Payment() {
   const [{ basket, user }, dispatch] = useStateValue();
@@ -11,13 +13,34 @@ function Payment() {
   const stripe = useStripe();
   const elements = useElements();
 
+  const [succeeded, setSucceeded] = useState(false);
+  const [processing, setProcessing] = useState("");
 
+  const [error, setError] = useState(null);
+  const [disabled, setDisabled] = useState(true);
+
+  const handleSubmit = (e) => {
+    // do all the fancy stripe stuff
+  };
+
+  const handleChange = (event) => {
+    // Listen for changes in the CardElement
+    // Display any errors as the customer types their card details
+
+    // we are going to listen for changes inside the card element and display any errors as the customer types their card details
+
+    // if the event is empty, then disable the button, otherwise, if there is an error show the error, otherwise show nothing !!!!
+
+    setDisabled(event.empty);
+    setError(event.error ? event.error.message : "");
+  };
 
   return (
     <div className="payment">
       <div className="payment__container">
-
-        <h1>Checkout (<Link to="/checkout">{basket?.length} items</Link>)</h1>
+        <h1>
+          Checkout (<Link to="/checkout">{basket?.length} items</Link>)
+        </h1>
 
         {/* payment section - delivery address */}
         <div className="payment__section">
@@ -32,39 +55,55 @@ function Payment() {
         </div>
         {/* payment section - review items*/}
         <div className="payment__section">
-        <div className="payment__title">
-          <h3>Review Items & Delivery</h3>
+          <div className="payment__title">
+            <h3>Review Items & Delivery</h3>
+          </div>
+          <div className="payment__items">
+            {basket.map((item) => (
+              <CheckoutProduct
+                id={item.id}
+                title={item.title}
+                image={item.image}
+                price={item.price}
+                rating={item.rating}
+              />
+            ))}
+          </div>
         </div>
-        <div className="payment__items">
-          {basket.map((item) => (
-            <CheckoutProduct
-              id={item.id}
-              title={item.title}
-              image={item.image}
-              price={item.price}
-              rating={item.rating}
-            />
-          ))}
-          </div>
-          </div>
 
         {/* payment section - payment method*/}
         <div className="payment__section">
-        <div className="payment__title">
-          <h3>Payment Method</h3>
-        </div>
-        <div className="payment__details">
-          
-            
-            <form>
-              <CardElement />
+          <div className="payment__title">
+            <h3>Payment Method</h3>
+          </div>
+          <div className="payment__details">
+            <form onSubmit={handleSubmit}>
+              <CardElement onChange={handleChange} />
+
+              <div className="payment__priceContainer">
+                <CurrencyFormat
+                  renderText={(value) => <h3>Order Total: {value}</h3>}
+                  decimalScale={2}
+                  value={getBasketTotal(basket)}
+                  displayType={"text"}
+                  thousandSeparator={true}
+                  prefix={"$"}
+                />
+                <button disabled={processing || disabled || succeeded}>
+                  <span>{processing ? <p>Processing</p> : "Buy Now"}</span>
+
+                </button>
+
+              </div>
+              {error && <div>{error}</div>}
+
             </form>
 
-          {/* Stripe Magic */}</div>
-        
+            {/* Stripe Magic */}
+          </div>
+        </div>
       </div>
-      </div>
-      </div>
+    </div>
   );
 }
 
