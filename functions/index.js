35c14cf11eef01e7d469784e2ff3d58937e72dca
bianca916 <59674024+bianca916@users.noreply.github.com>
2020-10-14 12:51:16 -0400

    initializing backend

--- functions/index.js
+++ functions/index.js
@@ -1,30 +1,13 @@
 const functions = require('firebase-functions');
 
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+const functions = require('firebase-functions');
 const express = require("express");
-
 const cors = require("cors");
-const { AppsSharp } = require('@material-ui/icons');
-
-const stripe = require("stripe")('sk_test_51HUNG6Fh9Y9psLFszb217vxCXSWbySMgDZkIgMYZnjst5i8yGgnuhCTwMu5KUjK2OculwQ25lwMDNnn0XmFmG2ql00Wl85O5vv');
-
-//  API
-
-// App config
-
-const app = express();
-
-
-// Middlewares
-
-app.use(cors({ origin: true }));
-
-app.use(express.json());
-
-// API Routes
-
-app.get('/', (request, response) => response.status(200).send('hello world'))
-
-// Listen Command
-
-exports.api = functions.https.onRequest(app)
-
+const stripe = require("stripe")('sk_test_51HUNG6Fh9Y9psLFszb217vxCXSWbySMgDZkIgMYZnjst5i8yGgnuhCTwMu5KUjK2OculwQ25lwMDNnn0XmFmG2ql00Wl85O5vv')
